3068dbc94018fed6aa466b840722005f5f30ae2f
Brian Ray <bray_stenovations@yahoo.com>
2024-09-10 18:23:15 -0400

    added text box processing

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,72 +28,159 @@ partial class frmMain
     /// </summary>
     private void InitializeComponent()
     {
-        btnHello = new Button();
-        lblStatus = new Label();
-        btnShow = new Button();
-        btnClose = new Button();
+        firstNameLabel = new Label();
+        firstNameTextBox = new TextBox();
+        lastNameLabel = new Label();
+        lastNameTextBox = new TextBox();
+        concatButton = new Button();
+        statusLabel = new Label();
+        hoursWorkedLabel = new Label();
+        hoursWorkedTextBox = new TextBox();
+        rateTextBox = new Label();
+        hourlyRateTextBox = new TextBox();
+        computeButton = new Button();
+        demoButton = new Button();
         SuspendLayout();
         // 
-        // btnHello
+        // firstNameLabel
         // 
-        btnHello.Location = new Point(497, 250);
-        btnHello.Name = "btnHello";
-        btnHello.Size = new Size(222, 93);
-        btnHello.TabIndex = 1;
-        btnHello.Text = "Hello World";
-        btnHello.UseVisualStyleBackColor = true;
-        btnHello.Click += btnHello_Click;
+        firstNameLabel.AutoSize = true;
+        firstNameLabel.Location = new Point(12, 13);
+        firstNameLabel.Name = "firstNameLabel";
+        firstNameLabel.Size = new Size(80, 20);
+        firstNameLabel.TabIndex = 0;
+        firstNameLabel.Text = "&First Name";
         // 
-        // lblStatus
+        // firstNameTextBox
         // 
-        lblStatus.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        lblStatus.ForeColor = Color.OrangeRed;
-        lblStatus.Location = new Point(50, 37);
-        lblStatus.Name = "lblStatus";
-        lblStatus.Size = new Size(447, 72);
-        lblStatus.TabIndex = 1;
-        lblStatus.Text = "test";
+        firstNameTextBox.Location = new Point(12, 45);
+        firstNameTextBox.Name = "firstNameTextBox";
+        firstNameTextBox.Size = new Size(279, 27);
+        firstNameTextBox.TabIndex = 1;
         // 
-        // btnShow
+        // lastNameLabel
         // 
-        btnShow.Location = new Point(138, 320);
-        btnShow.Name = "btnShow";
-        btnShow.Size = new Size(216, 75);
-        btnShow.TabIndex = 0;
-        btnShow.Text = "Show/Hide";
-        btnShow.UseVisualStyleBackColor = true;
-        btnShow.Click += btnShow_Click;
+        lastNameLabel.AutoSize = true;
+        lastNameLabel.Location = new Point(297, 13);
+        lastNameLabel.Name = "lastNameLabel";
+        lastNameLabel.Size = new Size(79, 20);
+        lastNameLabel.TabIndex = 2;
+        lastNameLabel.Text = "&Last Name";
         // 
-        // btnClose
+        // lastNameTextBox
         // 
-        btnClose.Location = new Point(430, 56);
-        btnClose.Name = "btnClose";
-        btnClose.Size = new Size(248, 112);
-        btnClose.TabIndex = 2;
-        btnClose.Text = "Close";
-        btnClose.UseVisualStyleBackColor = true;
-        btnClose.Click += btnClose_Click;
+        lastNameTextBox.Location = new Point(297, 45);
+        lastNameTextBox.Name = "lastNameTextBox";
+        lastNameTextBox.Size = new Size(243, 27);
+        lastNameTextBox.TabIndex = 3;
+        // 
+        // concatButton
+        // 
+        concatButton.Location = new Point(546, 45);
+        concatButton.Name = "concatButton";
+        concatButton.Size = new Size(94, 29);
+        concatButton.TabIndex = 4;
+        concatButton.Text = "&Concat";
+        concatButton.UseVisualStyleBackColor = true;
+        concatButton.Click += concatButton_Click;
+        // 
+        // statusLabel
+        // 
+        statusLabel.BorderStyle = BorderStyle.FixedSingle;
+        statusLabel.Location = new Point(12, 339);
+        statusLabel.Name = "statusLabel";
+        statusLabel.Size = new Size(628, 136);
+        statusLabel.TabIndex = 5;
+        statusLabel.TextAlign = ContentAlignment.MiddleCenter;
+        // 
+        // hoursWorkedLabel
+        // 
+        hoursWorkedLabel.AutoSize = true;
+        hoursWorkedLabel.Location = new Point(12, 206);
+        hoursWorkedLabel.Name = "hoursWorkedLabel";
+        hoursWorkedLabel.Size = new Size(103, 20);
+        hoursWorkedLabel.TabIndex = 6;
+        hoursWorkedLabel.Text = "Hours &Worked";
+        // 
+        // hoursWorkedTextBox
+        // 
+        hoursWorkedTextBox.Location = new Point(12, 229);
+        hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+        hoursWorkedTextBox.Size = new Size(279, 27);
+        hoursWorkedTextBox.TabIndex = 7;
+        // 
+        // rateTextBox
+        // 
+        rateTextBox.AutoSize = true;
+        rateTextBox.Location = new Point(297, 206);
+        rateTextBox.Name = "rateTextBox";
+        rateTextBox.Size = new Size(87, 20);
+        rateTextBox.TabIndex = 8;
+        rateTextBox.Text = "Hourly &Rate";
+        // 
+        // hourlyRateTextBox
+        // 
+        hourlyRateTextBox.Location = new Point(297, 229);
+        hourlyRateTextBox.Name = "hourlyRateTextBox";
+        hourlyRateTextBox.Size = new Size(243, 27);
+        hourlyRateTextBox.TabIndex = 9;
+        // 
+        // computeButton
+        // 
+        computeButton.Location = new Point(546, 229);
+        computeButton.Name = "computeButton";
+        computeButton.Size = new Size(94, 29);
+        computeButton.TabIndex = 10;
+        computeButton.Text = "Compute";
+        computeButton.UseVisualStyleBackColor = true;
+        computeButton.Click += computeButton_Click;
+        // 
+        // demoButton
+        // 
+        demoButton.Location = new Point(231, 123);
+        demoButton.Name = "demoButton";
+        demoButton.Size = new Size(179, 29);
+        demoButton.TabIndex = 11;
+        demoButton.Text = "Value Ref Demo";
+        demoButton.UseVisualStyleBackColor = true;
+        demoButton.Click += demoButton_Click;
         // 
         // frmMain
         // 
         AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
-        ControlBox = false;
-        Controls.Add(btnClose);
-        Controls.Add(btnShow);
-        Controls.Add(lblStatus);
-        Controls.Add(btnHello);
+        ClientSize = new Size(660, 493);
+        Controls.Add(demoButton);
+        Controls.Add(computeButton);
+        Controls.Add(hourlyRateTextBox);
+        Controls.Add(rateTextBox);
+        Controls.Add(hoursWorkedTextBox);
+        Controls.Add(hoursWorkedLabel);
+        Controls.Add(statusLabel);
+        Controls.Add(concatButton);
+        Controls.Add(lastNameTextBox);
+        Controls.Add(lastNameLabel);
+        Controls.Add(firstNameTextBox);
+        Controls.Add(firstNameLabel);
         Name = "frmMain";
         Text = "Our First Program";
         Load += Form1_Load;
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
 
-    private Button btnHello;
-    private Label lblStatus;
-    private Button btnShow;
-    private Button btnClose;
+    private Label firstNameLabel;
+    private TextBox firstNameTextBox;
+    private Label lastNameLabel;
+    private TextBox lastNameTextBox;
+    private Button concatButton;
+    private Label statusLabel;
+    private Label hoursWorkedLabel;
+    private TextBox hoursWorkedTextBox;
+    private Label rateTextBox;
+    private TextBox hourlyRateTextBox;
+    private Button computeButton;
+    private Button demoButton;
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -12,19 +12,194 @@ public partial class frmMain : Form
 
     }
 
-    private void btnHello_Click(object sender, EventArgs e)
+    private void concatButton_Click(object sender, EventArgs e)
     {
-        lblStatus.Text = "changed2";
+        var firstName = firstNameTextBox.Text;
+        string lastName = lastNameTextBox.Text;
+        string fullName = firstName + " " + lastName;
+
+        statusLabel.Text = fullName;
     }
 
-    private void btnShow_Click(object sender, EventArgs e)
+    // value vs. reference params
+    class ParamTester
     {
-        lblStatus.Visible = !lblStatus.Visible;
+        public int x;
     }
 
-    private void btnClose_Click(object sender, EventArgs e)
+    private void demoButton_Click(object sender, EventArgs e)
     {
-        this.Close();
-        // this is a change
+        ValueRefDemo1();
+        //ValueRefDemo2();
+        //ValueRefDemo3();
+        //ValueRefDemo4();
+        //NullErrorDemo();
+        //NullCrashDemo();
+        //NullableValueDemo();
     }
+
+    private void ValueRefDemo1()
+    {
+        int x = 1;
+        
+        MessageBox.Show(x.ToString());
+        ValueParam(x);
+        MessageBox.Show(x.ToString());
+    }
+
+    private void ValueRefDemo2()
+    {
+        ParamTester paramTester = new ParamTester();
+        paramTester.x = 30;
+        
+        MessageBox.Show(paramTester.x.ToString());
+        ObjParam(paramTester);
+        MessageBox.Show(paramTester.x.ToString());
+    }
+
+    private void ValueRefDemo3()
+    {
+        string str = "init value";
+
+        MessageBox.Show(str);
+        ValueParam(str);
+        MessageBox.Show(str);
+    }
+
+    private void ValueRefDemo4()
+    {
+        int x = 3;
+
+        MessageBox.Show(x.ToString());
+        RefParam(ref x);
+        MessageBox.Show(x.ToString());
+    }
+
+    private void ValueParam(int x)
+    {
+        x = 3;
+    }
+
+    private void ValueParam(string str)
+    {
+        str = "set by RefParam function";
+    }
+
+    private void ObjParam(ParamTester pt)
+    {
+        pt.x = 45;
+    }
+
+    private void RefParam(ref int x)
+    {
+        x = 7;
+    }
+
+    // a null warning
+    private void NullErrorDemo()
+    {
+        var firstName = firstNameTextBox.Text;
+        string lastName = lastNameTextBox.Text;
+        String fullName;
+        System.String sameThing = "";
+
+        // comment this out to show error
+        fullName = firstName + " " + lastName;
+
+        statusLabel.Text = fullName;
+    }
+
+    // crash by using stringVariable.Length.ToString()
+    private void NullCrashDemo()
+    {
+        string crash = null;
+        //string? crash = null;
+
+        statusLabel.Text = crash.ToString();
+
+        //if (!string.IsNullOrEmpty(crash))
+        //{
+        //    statusLabel.Text = crash.ToString();
+        //}
+
+        //if (crash is not null)
+        //{
+        //    statusLabel.Text = crash.ToString();
+        //}
+
+        //if (crash != null)
+        //{
+        //    statusLabel.Text = crash.ToString();
+        //}
+
+        // null conditional operator
+        statusLabel.Text = crash?.ToString();
+
+        // null coalescing operator
+        statusLabel.Text = crash ?? "null coalescing operator";
+    }
+
+    private void NullableValueDemo()
+    {
+        int? x = null;
+
+        //x = 1;
+        int y = x ?? 3;
+        MessageBox.Show(y.ToString());
+    }
+
+
+
+    private void computeButton_Click(object sender, EventArgs e)
+    {
+        CalculateDollars();
+        //CalculateEuros();
+        //CalculateWithExceptionHandling();
+    }
+
+    private void CalculateDollars()
+    {
+        // uncomment this and comment next to show error
+        //int hoursWorked = hoursWorkedTextBox.Text;
+        int hoursWorked = int.Parse(hoursWorkedTextBox.Text);
+
+        //decimal hourlyRate = hourlyRateTextBox.Text;
+        decimal hourlyRate = decimal.Parse(hourlyRateTextBox.Text);
+
+        // typecasting
+        double total = (double)(hoursWorked * hourlyRate);
+
+        // formatting pg. 150-152
+        //statusLabel.Text = total;
+        statusLabel.Text = total.ToString();
+    }
+
+    private void CalculateEuros()
+    {
+        int hoursWorked = int.Parse(hoursWorkedTextBox.Text);
+        decimal hourlyRate = decimal.Parse(hourlyRateTextBox.Text);
+        double dollars = (double)(hoursWorked * hourlyRate);
+        double euros = dollars * CONVERSION_RATE;
+        
+        // string interpolation
+        // formatting pg. 150-152
+        statusLabel.Text = $"we earned {euros} euros.";
+    }
+
+    // with exception handling
+    private void CalculateWithExceptionHandling()
+    {
+        try
+        {
+            CalculateDollars();
+        }
+        catch (Exception ex)
+        {
+            statusLabel.ForeColor = Color.Red;
+            statusLabel.Text = ex.Message;
+        }
+    }
+
+    private const double CONVERSION_RATE = 0.91;
+    private readonly decimal CONVERSION_RATE2 = 0.91m;
 }
